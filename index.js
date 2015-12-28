bd5da6c1a41e3bf46cf864c91a58d747fbda4d73
Vladimir Kudinov <frux@yandex-team.ru>
2015-12-28 17:01:50 +0500

    Ping

--- index.js
+++ index.js
@@ -1,3 +1,90 @@
-/**
- * Created by frux on 28/12/15.
- */
+'use strict';
+
+const got = require('got');
+const path = require('path');
+const seenk = require('seenk');
+const chalk = require('chalk');
+
+function runConfig(configPath, sync){
+	let checks,
+		config;
+
+	if(!path.isAbsolute(configPath)){
+		configPath = path.resolve(process.cwd(), configPath);
+	}
+
+	config = require(configPath);
+
+	if(config.length){
+		if(sync){
+			return seenk(function*(){
+				for(let check of config){
+					yield ping(check.ping, check.expected)
+						.then(() =>{
+							_logPing({ url, status: 'ok' }, check.name);
+						})
+						.catch(err => {
+							_logPing(err, check.name);
+						});
+				}
+			});
+		}else{
+			checks = config.map(check => {
+				return ping(check.ping, check.expected)
+					.then(() =>{
+						_logPing({ url: check.ping, status: 'ok' }, check.name);
+					})
+					.catch(err => {
+						_logPing(err, check.name);
+					});
+			});
+
+			return seenk(checks);
+		}
+	}else return Promise.resolve(true);
+}
+
+function _checkStatusCode(expected, received, url){
+	if(received !== expected){
+		return {
+			action: 'ping',
+			url,
+			expected,
+			received,
+			status: 'error',
+			error: 'Unexpected status code'
+		};
+	}else{
+		return true;
+	}
+}
+
+function ping(url, expectedStatucCode){
+	isNaN(expectedStatucCode) && (expectedStatucCode = 200);
+
+	return new Promise((resolve, reject) => {
+		got(url, (error, body, response) => {
+			const receivedStatusCode = (error ? error.statusCode: response.statusCode);
+			const result = _checkStatusCode(expectedStatucCode, receivedStatusCode, url);
+
+			if(result === true){
+				resolve(result);
+			}else{
+				reject(result);
+			}
+		});
+	});
+}
+
+function _logPing(data, name){
+	if(data.status === 'error'){
+		console.log(`[${name || `ping ${data.url}`}] ${chalk.red(`FAILED ${data.error}; Received ${data.received} Expected ${data.expected}`)}`);
+	}else{
+		console.log(`[${name || `ping ${data.url}`}] ${chalk.green(`OK`)}`);
+	}
+}
+
+module.exports = {
+	ping,
+	runConfig
+};
--- test/index.js
+++ test/index.js
@@ -1,7 +1,67 @@
 const assert = require('assert');
+const Gerkon = require('gerkon');
+const vermont = require('../');
 
-describe('Test', () => {
-	it('should be ok', () => {
-		assert.ok(true, true);
+process.chdir(__dirname);
+
+describe('Ping', () => {
+
+	Gerkon
+		.param('port', 3163)
+		//.param('logs', 'string')
+		.route('/ping/test1', (req, res) => res.send('pong'))
+		.route('/ping/test2', (req, res) => res.sendCode(500))
+		.route('/ping/test3', (req, res) => res.send('pong'))
+		.init();
+
+	it('should be ok if server returns expected status code', done => {
+		vermont.ping('http://localhost:3163/ping/test1')
+			.then(() => {
+				done();
+			})
+			.catch(err => assert.fail(err));
+	});
+
+	it('should not be ok if server returns unexpected status code', done => {
+		vermont.ping('http://localhost:3163/ping/test2', 200)
+			.then((result) => {
+				if(result !== true){
+					assert.equal(result.error, 'Unexpected status code');
+					done();
+				}
+			})
+			.catch(() => {
+				done();
+			});
+	});
+
+	it('should runs checkings from config (abs path)', done => {
+		vermont.runConfig(__dirname + '/vermont.json')
+			.then(() => done())
+			.catch(err => assert.fail(err));
+	});
+
+	it('should runs checkings from config (rel path)', done => {
+		vermont.runConfig('./vermont.json')
+			.then(() => done())
+			.catch(err => assert.fail(err));
+	});
+
+	it('should runs checkings synchronously', done => {
+		vermont.runConfig('./vermont.json', true)
+			.then(() => done())
+			.catch(err => assert.fail(err));
+	});
+
+	it('should throws if any check hasn`t passed', done => {
+		vermont.runConfig('./vermont.bad.json')
+			.then(() => assert.fail())
+			.catch(err => done());
+	});
+
+	it('should not fail if config is empty', done => {
+		vermont.runConfig('./vermont.empty.json')
+			.then(() => done())
+			.catch(err => assert.fail(err));
 	});
 });
