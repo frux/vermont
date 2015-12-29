a821f1f46ef7828990761b327cbf07658c221e0a
Vladimir Kudinov <frux@yandex-team.ru>
2015-12-29 17:56:13 +0500

    Exec + tests

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 const got = require('got');
 const path = require('path');
+const child = require('child_process');
 const seenk = require('seenk');
 const chalk = require('chalk');
+const actions = {
+	ping,
+	exec
+};
 
 function runConfig(configPath, sync){
 	let checks,
@@ -19,72 +24,148 @@ function runConfig(configPath, sync){
 		if(sync){
 			return seenk(function*(){
 				for(let check of config){
-					yield ping(check.ping, check.expected)
-						.then(() =>{
-							_logPing({ url, status: 'ok' }, check.name);
+					let action = _getAction(check);
+
+					yield actions[action](check[action], check.expected)
+						.then(() => {
+							_log({ status: 'ok' }, check.name);
 						})
 						.catch(err => {
-							_logPing(err, check.name);
+							_log(err, check.name);
 						});
 				}
 			});
 		}else{
-			checks = config.map(check => {
-				return ping(check.ping, check.expected)
-					.then(() =>{
-						_logPing({ url: check.ping, status: 'ok' }, check.name);
-					})
-					.catch(err => {
-						_logPing(err, check.name);
-					});
-			});
+			return seenk(function*(){
+				yield config.map(check => {
+					let action = _getAction(check);
 
-			return seenk(checks);
+					return actions[action](check[action], check.expected)
+						.then(() => {
+							_log({ status: 'ok' }, check.name);
+						})
+						.catch(err => {
+							_log(err, check.name);
+							throw err;
+						});
+				});
+			});
 		}
 	}else return Promise.resolve(true);
 }
 
-function _checkStatusCode(expected, received, url){
-	if(received !== expected){
-		return {
-			action: 'ping',
-			url,
-			expected,
-			received,
-			status: 'error',
-			error: 'Unexpected status code'
-		};
+function _getAction(check){
+	if(check.ping){
+		return 'ping';
 	}else{
-		return true;
+		return 'exec';
 	}
 }
 
-function ping(url, expectedStatucCode){
-	isNaN(expectedStatucCode) && (expectedStatucCode = 200);
+function _check(expected, received){
+	let passed;
+
+	if(typeof expected === 'number'){
+		passed = (received === expected);
+	}else{
+		passed = new RegExp(expected).test(received);
+	}
+
+	if(passed){
+		return true;
+	}else{
+		return {
+			expected,
+			received,
+			status: 'error'
+		};
+	}
+}
+
+function ping(url, expected){
+
+	//param expected might be either number or regular expression
+	//if number provided Vermont will compare it with statusCode else with response body
+	if(typeof expected !== 'number' && typeof expected !== 'string'){
+		expected = 200;
+	}
 
 	return new Promise((resolve, reject) => {
 		got(url, (error, body, response) => {
-			const receivedStatusCode = (error ? error.statusCode: response.statusCode);
-			const result = _checkStatusCode(expectedStatucCode, receivedStatusCode, url);
+			let received,
+				result;
+
+			//if number expected Vermont will compare it with statusCode
+			if(typeof expected === 'number'){
+				received = (error || response).statusCode;
+
+			//else with body
+			}else{
+				received = body || '';
+			}
+
+			result = _check(expected, received);
 
 			if(result === true){
 				resolve(result);
 			}else{
+				result.url = url;
+				result.action = 'ping';
 				reject(result);
 			}
 		});
 	});
 }
 
-function _logPing(data, name){
-	if(data.status === 'error'){
-		console.log(`[${name || `ping ${data.url}`}] ${chalk.red(`FAILED ${data.error}; Received ${data.received} Expected ${data.expected}`)}`);
-	}else{
-		console.log(`[${name || `ping ${data.url}`}] ${chalk.green(`OK`)}`);
+function exec(command, expected){
+
+	//param expected might be either number or string or regular expression
+	//if number provided Vermont will compare it with exit code else with stdout
+	if(typeof expected !== 'number' && typeof expected !== 'string'){
+		expected = 0;
 	}
+
+	return new Promise((resolve, reject) => {
+		child.exec(command, (error, stdout, stderr) => {
+			let received,
+				result;
+
+			//if number expected Vermont will compare it with exit code
+			if(typeof expected === 'number'){
+				received = (error ? error.code : 0);
+
+			//else with stdout
+			}else{
+				received = stdout.toString().replace(/(^\n+|\n+$)/g, '') || '';
+			}
+
+			result = _check(expected, received);
+
+			if(result === true){
+				resolve(result);
+			}else{
+				result.command = command;
+				result.action = 'exec';
+				reject(result);
+			}
+		});
+	});
+}
+
+function _log(data, actionName){
+	let log = `[${actionName}] `;
+
+	if(data.status === 'error'){
+		log += `${chalk.red(`FAILED! Received ${data.received} Expected ${data.expected}`)}`;
+	}else{
+		log += chalk.green('OK');
+	}
+
+	console.log(log);
 }
 
 module.exports = {
 	ping,
+	exec,
 	runConfig
 };
--- test/index.js
+++ test/index.js
@@ -1,67 +1,159 @@
 const assert = require('assert');
+const should = require('should');
 const Gerkon = require('gerkon');
 const vermont = require('../');
 
 process.chdir(__dirname);
 
 describe('Ping', () => {
+	const host = 'http://localhost:3163';
 
 	Gerkon
 		.param('port', 3163)
 		//.param('logs', 'string')
-		.route('/ping/test1', (req, res) => res.send('pong'))
-		.route('/ping/test2', (req, res) => res.sendCode(500))
-		.route('/ping/test3', (req, res) => res.send('pong'))
+		.route('/200', (req, res) => res.sendCode(200))
+		.route('/502', (req, res) => res.sendCode(502))
+		.route('/pong', (req, res) => res.send('pong'))
 		.init();
 
-	it('should be ok if server returns expected status code', done => {
-		vermont.ping('http://localhost:3163/ping/test1')
-			.then(() => {
-				done();
-			})
-			.catch(err => assert.fail(err));
+	it('should expect default statusCode 200 if `expected` param is undefined', (done) => {
+		vermont.ping(`${host}/200`)
+			.then(() => done())
+			.catch(err => assert.fail(err.expected, err.received));
 	});
 
-	it('should not be ok if server returns unexpected status code', done => {
-		vermont.ping('http://localhost:3163/ping/test2', 200)
-			.then((result) => {
-				if(result !== true){
-					assert.equal(result.error, 'Unexpected status code');
-					done();
-				}
-			})
-			.catch(() => {
+	it('should compare with statusCode if `expected` param is a number', (done) => {
+		vermont.ping(`${host}/200`, 200)
+			.then(() => done())
+			.catch(err => assert.fail(err.expected, err.received));
+	});
+
+	it('should compare with body if `expected` param is a string', (done) => {
+		vermont.ping(`${host}/pong`, 'pong')
+			.then(() => done())
+			.catch(err => assert.fail(err.expected, err.received));
+	});
+
+	it('should compare `expected` param as a regular expression', (done) => {
+		vermont.ping(`${host}/pong`, '^[a-z]{4}$')
+			.then(() => done())
+			.catch(err => assert.fail(err.expected, err.received));
+	});
+
+	it('should not crash if server`s answer is empty', (done) => {
+		vermont.ping(`${host}/200`, '^$')
+			.then(() => done())
+			.catch(err => assert.fail(err.expected, err.received));
+	});
+
+	it('should catch error if check has not passed', (done) => {
+		vermont.ping(`${host}/502`, 200)
+			.then(() => assert.fail())
+			.catch(err => {
+				err.should.eql({
+					status: 'error',
+					action: 'ping',
+					url: `${host}/502`,
+					expected: 200,
+					received: 502
+				});
+
 				done();
 			});
 	});
+});
 
-	it('should runs checkings from config (abs path)', done => {
-		vermont.runConfig(__dirname + '/vermont.json')
+describe('Exec', () => {
+	it('should expect default exit code 0 if `expected` param is undefined', (done) => {
+		vermont.exec('sh -c "exit 0;"')
 			.then(() => done())
-			.catch(err => assert.fail(err));
+			.catch(err => assert.fail(err.expected, err.received));
 	});
 
-	it('should runs checkings from config (rel path)', done => {
+	it('should compare with statusCode if `expected` param is a number', (done) => {
+		vermont.exec('sh -c "exit 1;"', 1)
+			.then(() => done())
+			.catch(err => assert.fail(err.expected, err.received));
+	});
+
+	it('should compare with body if `expected` param is a string', (done) => {
+		vermont.exec('echo test', 'test')
+			.then(() => done())
+			.catch(err => assert.fail(err.expected, err.received));
+	});
+
+	it('should compare `expected` param as a regular expression', (done) => {
+		vermont.exec(`echo test`, '^[a-z]{4}$')
+			.then(() => done())
+			.catch(err => assert.fail(err.expected, err.received));
+	});
+
+	it('should not crash if server`s answer is empty', (done) => {
+		vermont.exec('sh -c "exit 0;"', '^$')
+			.then(() => done())
+			.catch(err => assert.fail(err.expected, err.received));
+	});
+
+	it('should catch error if check has not passed', (done) => {
+		vermont.exec(`echo foo`, 'bar')
+			.then(() => assert.fail())
+			.catch(err => {
+				err.should.eql({
+					status: 'error',
+					action: 'exec',
+					command: 'echo foo',
+					expected: 'bar',
+					received: 'foo'
+				});
+
+				done();
+			});
+	});
+});
+
+describe('Configs', () => {
+	it('should run config if path is relative', done => {
 		vermont.runConfig('./vermont.json')
 			.then(() => done())
-			.catch(err => assert.fail(err));
+			.catch(err => assert.fail());
 	});
 
-	it('should runs checkings synchronously', done => {
-		vermont.runConfig('./vermont.json', true)
+	it('should run config if path is absolute', done => {
+		vermont.runConfig(`${__dirname}/vermont.json`)
 			.then(() => done())
-			.catch(err => assert.fail(err));
+			.catch(err => assert.fail());
 	});
 
-	it('should throws if any check hasn`t passed', done => {
-		vermont.runConfig('./vermont.bad.json')
+	it('should not fail if config has no checks', done => {
+		vermont.runConfig(`${__dirname}/vermont.empty.json`)
+			.then(() => done())
+			.catch(err => assert.fail());
+	});
+
+	it('should catch if any check has not passed (async)', done => {
+		vermont.runConfig(`${__dirname}/vermont.bad.json`)
 			.then(() => assert.fail())
 			.catch(err => done());
 	});
 
-	it('should not fail if config is empty', done => {
-		vermont.runConfig('./vermont.empty.json')
+	it('should run checks synchronously', done => {
+		vermont.runConfig(`${__dirname}/vermont.json`, true)
 			.then(() => done())
-			.catch(err => assert.fail(err));
+			.catch(err => assert.fail());
+	});
+
+	it('should catch if any check has not passed (sync)', done => {
+		vermont.runConfig(`${__dirname}/vermont.bad.json`, true)
+			.then(() => assert.fail())
+			.catch(err => done());
+	});
+});
+
+
+describe('General', () => {
+	it('should catch error if `expected` neither number or string', done => {
+		vermont.exec(`echo test`, [1, 2])
+			.then(() => done())
+			.catch(err => assert.eql(err, false));
 	});
 });
